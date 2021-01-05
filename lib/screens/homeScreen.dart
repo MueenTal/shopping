9d6cd5b931efa99a7cdcdc9ff4db0e43c5d179c9
mueental <mueentalla01@gmail.com>
2021-01-05 23:03:53 +0200

    ...

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -67,9 +67,19 @@ class _HomeScreenState extends State<HomeScreen> {
                   ),
                 ),
               ),
+              Divider(),
               Expanded(
                 child: ListView(
-                  children: [item(3.3, () {}, "mmm")],
+                  children: [
+                    item(3.3, () {}, "mmm"),
+                    item(3.3, () {}, "mmm"),
+                    item(3.3, () {}, "mmm"),
+                    item(3.3, () {}, "mmm"),
+                    item(3.3, () {}, "mmm"),
+                    item(3.3, () {}, "mmm"),
+                    item(3.3, () {}, "mmm"),
+                    item(3.3, () {}, "mmm"),
+                  ],
                 ),
               )
             ],
@@ -80,6 +90,97 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   Widget item(double price, Function function, String name) {
-    return Container();
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Container(
+          height: 60,
+          padding: EdgeInsets.all(3),
+          decoration: BoxDecoration(
+              boxShadow: [BoxShadow(spreadRadius: 2.6, color: Colors.grey)],
+              color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(10))),
+          child: Center(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Container(
+                  height: 60,
+                  width: MediaQuery.of(context).size.width / 2.4,
+                  color: Colors.white,
+                  child: Center(
+                    child: Text(
+                      name,
+                      style: TextStyle(fontSize: 17),
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 70,
+                  width: 4,
+                  color: Colors.grey[300],
+                ),
+                Container(
+                  height: 60,
+                  width: MediaQuery.of(context).size.width / 5,
+                  color: Colors.white,
+                  child: Center(
+                      child: Text(
+                    price.toString(),
+                    style: TextStyle(fontSize: 18),
+                  )),
+                ),
+                Container(
+                  height: 70,
+                  width: 4,
+                  color: Colors.grey[300],
+                ),
+                Container(
+                  height: 60,
+                  width: MediaQuery.of(context).size.width / 6,
+                  color: Colors.white,
+                  child: Center(
+                    child: IconButton(
+                      icon: Icon(Icons.delete),
+                      color: Colors.red,
+                      onPressed: () {
+                        showDialog(
+                            context: context,
+                            builder: (context) {
+                              return Dialog(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(30)),
+                                child: Container(
+                                  height: 350,
+                                  decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      boxShadow: [
+                                        BoxShadow(
+                                            spreadRadius: 1.3,
+                                            color: Colors.grey)
+                                      ],
+                                      borderRadius: BorderRadius.all(
+                                          Radius.circular(30))),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [],
+                                  ),
+                                ),
+                              );
+                            });
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
